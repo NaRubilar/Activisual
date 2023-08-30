102bf06d87760fd2cc8d361c78e735c255152019
Dieg026I <dieg.isla@duocuc.cl>
2023-08-30 00:01:08 -0400

    nosotrosPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+
+import { PageOneComponent } from './pages/nosotros/page-one.component';
+import { PageTwoComponent } from './pages/nosotros/page-two.component';
+import { PageThreeComponent } from './pages/nosotros/page-three.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PageOneComponent, PageTwoComponent, PageThreeComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/app/pages/nosotros/nosotros.page.ts
+++ src/app/pages/nosotros/nosotros.page.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PageOneComponent } from './page-one.component';
+
 @Component({
   selector: 'app-nosotros',
   templateUrl: './nosotros.page.html',
   styleUrls: ['./nosotros.page.scss'],
 })
 export class NosotrosPage implements OnInit {
+  component = PageOneComponent;
 
   constructor() { }
 
