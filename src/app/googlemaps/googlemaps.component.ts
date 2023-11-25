e39cb4e9ff13f02cc15cc234a2ce91627585bbbf
dieg026I <dieg.isla@duocuc.cl>
2023-11-24 23:40:17 -0300

    pruebas_marker

--- src/app/googlemaps/googlemaps.component.ts
+++ src/app/googlemaps/googlemaps.component.ts
@@ -2,13 +2,14 @@ import { DOCUMENT } from '@angular/common';
 import { Component, OnInit, Input, Renderer2, ElementRef, ViewChild, Inject } from '@angular/core';
 import { GooglemapsService } from './googlemaps.service';
 import { ModalController } from '@ionic/angular';
-
 import { Plugins } from '@capacitor/core';
 const {Geolocation} = Plugins;
 
 
 
 declare var google: any;
+
+
 
 
 @Component({
@@ -26,14 +27,21 @@ export class GooglemapsComponent implements OnInit {
       };
 
       label = {
-            titulo:'Ubicación',
-            subtitulo: 'Mi ubicación de envío'
+            titulo:'Estas Aquí',
+            subtitulo: 'Desubicado'
       } 
 
       map: any;
       marker: any;
+      markerView: any;
+      marker1: any;
+      marker2: any;
       infowindow: any;
-      positionSet: any
+      positionSet: any;
+      position1: any;
+      position2: any;
+      positionDuoc: any;
+      beachFlagMarkerView: any;
 
       @ViewChild('map') divMap: ElementRef;
 
@@ -46,6 +54,7 @@ export class GooglemapsComponent implements OnInit {
       ngOnInit(): void {
             this.init();
             console.log('position ->', this.position)
+              
       }
 
       async init() {
@@ -58,12 +67,10 @@ export class GooglemapsComponent implements OnInit {
             
       }
 
-
-
-
+      
       initMap() {
-
             const position = this.position;
+            this.positionDuoc = { lat: -33.033695220947266, lng: -71.53321075439453 };
 
             let latLng = new google.maps.LatLng(position.lat, position.lng);
 
@@ -74,22 +81,65 @@ export class GooglemapsComponent implements OnInit {
                   clickableIcons: false,
                   setClickableIcons: true,
             };
-
+           
             this.map = new google.maps.Map(this.divMap.nativeElement, mapOptions);
+            
             this.marker = new google.maps.Marker({
                   map: this.map,
                   animation: google.maps.Animation.DROP,
                   draggable: false,
-                  panTo: latLng
+                  panTo: latLng,
+                  icon: {
+                        url: 'assets/icon/favicon.png'
+
+                        }
             });
             //this.clickHandleEvent();
+            
             this.infowindow = new google.maps.InfoWindow();
-            this.addMarker(position);
+            
+            this.addMarker(this.position);
             this.setInfoWindow(this.marker, this.label.titulo, this.label.subtitulo);
+            
+            this.markerView = new google.maps.Marker({
+                  map:this.map,
+                  position: this.positionDuoc,
 
+            })
+            this.addMarkers(this.positionDuoc)
+            
+            // Jardin Botanico -33.048674782518226, -71.49779906163025
+            this.position1 = {
+                  lat: -33.048674782518226,
+                  lng: -71.49779906163025
+            };
+            
+            this.marker1 = new google.maps.Marker({
+                  map: this.map,
+                  animation: google.maps.Animation.DROP,
+                  draggable: false,
+                  position: this.position1,
+                
+            })
+            this.addMarkers(this.position1)
+             
+            //Cerro Alegre Valparaiso -33.043806258896325, -71.62657360068589 
+            this.position2 = {
+                  lat: -33.043806258896325,
+                  lng: -71.62657360068589
+            };
+            
+            this.marker2 = new google.maps.Marker({
+                  map: this.map,
+                  animation: google.maps.Animation.DROP,
+                  draggable: false,
+                  position: this.position2
+            });
+            this.addMarkers(this.position2)
+                
       }
-
-      /*clickHandleEvent() {
+      
+      clickHandleEvent() {
 
             this.map.addListener('click', (event: any) => {
                   const position = {
@@ -100,9 +150,7 @@ export class GooglemapsComponent implements OnInit {
             });
 
       }
-      */
-
-
+      
       addMarker(position: any): void {
 
             let latLng = new google.maps.LatLng(position.lat, position.lng);
@@ -112,8 +160,30 @@ export class GooglemapsComponent implements OnInit {
             this.positionSet = position;
 
       }
+      addMarkers(position: any): void {
 
+            if(position == this.position1){
 
+                  let latLng = new google.maps.LatLng(position.lat, position.lng);
+                  this.marker1.setPosition(latLng);
+                  this.positionSet = position;
+            
+            }else if (position == this.position2) {
+                  let latLng = new google.maps.LatLng(position.lat, position.lng)
+                  this.marker2.setPosition(latLng);
+                  this.positionSet = position;
+           
+            }else if (position == this.positionDuoc) {
+                  let latLng = new google.maps.LatLng(position.lat, position.lng)
+                  this.markerView.setPosition(latLng);
+                  this.positionSet = position;
+
+            }else{
+                  console.log('error en esta wea ')
+            }
+            
+
+      }
       setInfoWindow(marker: any, titulo: string, subtitulo: string) {
 
             const contentString  =  '<div id="contentInsideMap">' +
@@ -129,7 +199,8 @@ export class GooglemapsComponent implements OnInit {
             this.infowindow.open(this.map, marker);
 
       }
-
+      
+      
       async mylocation() {
             console.log('mylocation() click');
         
@@ -144,8 +215,8 @@ export class GooglemapsComponent implements OnInit {
                     lat: position.coords.latitude,
                     lng: position.coords.longitude,
                 };
-        
                 this.addMarker(markerPosition);
+                this.map.myLocationEnabled = true;
             } catch (error) {
                 console.error('Error getting current position:', error);
             }
